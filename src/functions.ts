493fa8858a17e6a01d24ef42fb3782e350862774
ska <saikrishna.learning@gmail.com>
2026-05-01 11:04:02 +0530

    functions

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,5 @@
+//usual functions
+
 function sayHello(): void{
     console.log("hello");
 }
@@ -22,3 +24,27 @@ function providingOptionalParameters(name: string, age?: number) : string{
 
 console.log(providingOptionalParameters("sai krishna"));
 console.log(providingOptionalParameters("sai krishna",42));
+
+
+//arrow functions
+const arithmeticArrow = (number1: number, number2: number): number => number1+number2;
+console.log(arithmeticArrow(10,10));
+
+//function overloading
+function combineDetails(value1: string, value2: string) : string;
+function combineDetails(val1: string, val2: number): number;
+function combineDetails(val1: number, val2: string): number;
+function combineDetails(val1: number, val2?: string): number;
+function combineDetails(...args: any[]): any;
+
+// function combineDetails(...args: any[]) : any {
+//     return args.reduce((acc, val) => acc + val, '');
+// }
+
+console.log(combineDetails("hello","100"));
+console.log(combineDetails("hello","anasinga"));
+console.log(combineDetails("hello",100));
+console.log(combineDetails(200,"100"));
+console.log(combineDetails(200,"100",300,"400"));
+console.log(combineDetails(200));
+
